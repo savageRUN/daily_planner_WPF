0c9889b24f992bae6b6bc6dfad2c8800ef16c64e
savageRUN <109674251+savageRUN@users.noreply.github.com>
2022-09-24 18:07:11 +0300

    Initial commit

---
+++ daily_planner_WPF/Models/PlannerModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daily_planner_WPF.Models
+{
+    class PlannerModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+
+        private string _text;
+
+
+        public bool isDone
+        {
+            get { return _isDone; }
+            set {
+                if (_isDone == value)
+                {
+                    return;
+                }
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set {
+                if (_text== value )
+                {
+                    return;
+                }
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            
+            
+        }
+    }
+}
---
+++ daily_planner_WPF/Services/FileIOService.cs
@@ -0,0 +1,45 @@
+﻿using daily_planner_WPF.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daily_planner_WPF.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<PlannerModel> LoadData()
+        {
+            var fileExsist = File.Exists(PATH);
+            if (!fileExsist)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<PlannerModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<PlannerModel>>(fileText);
+            }
+        }
+
+        public void SaveData(Object toDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(toDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
